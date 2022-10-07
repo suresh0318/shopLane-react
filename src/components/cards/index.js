130e12eba97136591326946a14c66a6bb379e59e
suresh0318 <sureshchoudhary616@gmail.com>
2022-10-07 13:42:59 +0530

    authentication bug fix

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -16,6 +16,7 @@ function Cards() {
     const result = await axios.get(
       "https://5d76bf96515d1a0014085cf9.mockapi.io/product"
     );
+    console.log(result)
     dispatch(data(result.data));
   };
 
