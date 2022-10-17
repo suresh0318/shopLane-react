5f64f6231625a78ce7da71812234270eac715bd6
suresh0318 <sureshchoudhary616@gmail.com>
2022-10-17 13:01:17 +0530

    quantity fix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { Auth0Provider } from "@auth0/auth0-react"
 import App from "./App";
 
+
+
 ReactDOM.render(
   <React.StrictMode>
    <Auth0Provider
-      domain={process.env.REACT_APP_DOMAIN}
-      clientId={process.env.REACT_APP_CLIENT_ID}
+      domain='suresh-website.us.auth0.com'
+      clientId='JrgpZmp4L17IUZGS9KX8I0IwUnGD0TT5'
       redirectUri={window.location.origin}
     >
       <App />
